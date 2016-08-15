30e1c2df0a6d9194012551db4fe5d9c2fdb3cd6e
Your Name <you@example.com>
2016-08-15 16:04:13 +0300

    Notes

--- js/controllers/tasks.js
+++ js/controllers/tasks.js
@@ -9,9 +9,10 @@
         { text: "מצב", css: "hidden", next: 0 }
     ]
 
-    function _clickTri(numplace) {
+    function _clickTri(id) {
         // Get task by place on the list:
-        var task = $scope.taskArray[numplace];
+        var task = _getTaskByID(id);
+        if (!task) return;
 
         for (var i = 0; i < TriStateFSM.length; i++) {
             if (task.tristate.css == TriStateFSM[i].css) {
@@ -25,13 +26,51 @@
         }
     }
 
-    $scope.OnTriClick = function (numplace) {
-        _clickTri(numplace);
+    $scope.OnTriClick = function (id) {
+        _clickTri(id);
+    }
+
+    /**************************************
+           NOTES
+   ***************************************/
+
+    $scope.AddNote = function (id) {
+        // Get task by place on the list:
+        var task = _getTaskByID(id);
+        if (!task) return;
+
+        // notes can be empty
+        if (task.notes) {
+            task.notes.push({
+                noteID: 12,
+                noteOwner: 'yoyo',
+                noteText: 'הכל סבבה'
+            });
+        }
+        else
+        {
+            task.notes = [{
+                noteID: 12,
+                noteOwner: 'yoyo',
+                noteText: 'הכל סבבה'
+            }];
+        }
     }
 
     /**************************************
            TASK ARRAY
    ***************************************/
+
+    function _getTaskByID(id) {
+        for (var i = 0; i < $scope.taskArray.length; i++) {
+            if ($scope.taskArray[i].taskID == id)
+                return $scope.taskArray[i];
+        }
+
+        // If we got here, no id is present:
+        console.log("tasks::_getTaskByID(id) cant find task with id:" + id);
+        return null;
+    }
 
     $scope.taskArray = [
         {
@@ -66,15 +105,19 @@
             notes: [
                 {
                     noteID: 2,
-                    noteOwner: 'מקשחר',
-                    noteText: 'הכל סבבה'
-                },
-                {
-                    noteID: 3,
-                    noteOwner: '2מקשחר',
-                    noteText: 'הכל סבבה2'
+                    noteOwner: 'מקש222חר',
+                    noteText: 'הכל סב<u>sss</u>בה'
                 },
             ]
+        },
+        {
+            taskID: 10,
+            favor: false,
+            description: 'Hello From the <u>pther</u><br /> side',
+            tristate: {
+                css: 'hidden',
+                text: 'מצב'
+            },
         },
     ];
 });
